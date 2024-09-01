7ecadd353bc20b83d893a91b01a7a75e53b4dbb5
Juan Pablo Vielma Valencia <74035090+Juanpablo9717@users.noreply.github.com>
2024-09-01 14:19:04 -0500

    Update Simplify.js with FIX en global scope

--- Simplify.js
+++ Simplify.js
@@ -1,37 +1,39 @@
-const Simplify = {
-  createUniqueStyles: ({ elementId, cssContent, pluginName }) => {
-    if (!pluginName || !cssContent || !pluginName) {
-      console.warn(
-        `IMPORTANT! remember to use all the props to correct functionality ${{
-          pluginName,
-          elementId,
-          cssContent,
-        }}`
-      );
-    }
+(function () {
+  if (!window.Simplify) {
+    window.Simplify = {
+      createUniqueStyles: ({ elementId, cssContent, pluginName }) => {
+        if (!pluginName || !cssContent || !pluginName) {
+          console.warn(
+            `IMPORTANT! remember to use all the props to correct functionality ${{
+              pluginName,
+              elementId,
+              cssContent,
+            }}`
+          );
+        }
 
-    const styleId = `${elementId}-element-styles-${pluginName}`;
-    let styleElement = document.getElementById(styleId);
+        const styleId = `${elementId}-element-styles-${pluginName}`;
+        let styleElement = document.getElementById(styleId);
 
-    if (styleElement) {
-      styleElement.innerHTML = cssContent;
-    } else {
-      styleElement = document.createElement("style");
-      styleElement.id = styleId;
-      styleElement.type = "text/css";
-      styleElement.innerHTML = cssContent;
-      document.head.appendChild(styleElement);
-    }
-  },
-  generateUUID: () => {
-    const timePart = Date.now().toString(36).substring(-2);
-    const randomPart = Math.random().toString(36).substring(2, 5);
-    return `${timePart}${randomPart}`;
-  },
-  extractNumericValue: (value) => {
-    if (!value) return 0;
-    return parseFloat(value) || 0;
-  },
-
-  // Add methods here!
-};
+        if (styleElement) {
+          styleElement.innerHTML = cssContent;
+        } else {
+          styleElement = document.createElement("style");
+          styleElement.id = styleId;
+          styleElement.type = "text/css";
+          styleElement.innerHTML = cssContent;
+          document.head.appendChild(styleElement);
+        }
+      },
+      generateUUID: () => {
+        const timePart = Date.now().toString(36).substring(-2);
+        const randomPart = Math.random().toString(36).substring(2, 5);
+        return `${timePart}${randomPart}`;
+      },
+      extractNumericValue: (value) => {
+        if (!value) return 0;
+        return parseFloat(value) || 0;
+      },
+    };
+  }
+})();
