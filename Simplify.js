b2b25e7ad95eedb628abb6c0191a2daddb9ca6be
Juan Pablo Vielma Valencia <74035090+Juanpablo9717@users.noreply.github.com>
2024-10-17 12:06:33 -0500

    Update Simplify.js extractDataFromListOfThings
    extractDataFromListOfThings

--- Simplify.js
+++ Simplify.js
@@ -34,6 +34,45 @@
         if (!value) return 0;
         return parseFloat(value) || 0;
       },
+
+      extractDataFromListOfThings: (list) => {
+        if (!list) {
+          console.error("List not found");
+          return;
+        }
+
+        const thingsLen = list.length();
+        // Return an array if list is empty
+        if (thingsLen === 0) return [];
+
+        // Extract all the elements from the list
+        const listOfThings = list.get(0, thingsLen);
+
+        // Set the size of the array from the length of the list
+        const result = new Array(thingsLen);
+
+        for (let i = 0; i < thingsLen; i++) {
+          const element = listOfThings[i];
+          const listOfProps = element.listProperties();
+          const thing = {};
+
+          for (let j = 0; j < listOfProps.length; j++) {
+            let prop = listOfProps[j];
+
+            // Remove the prefix "_p_" if it exists
+            const cleanProp = prop.startsWith("_p_")
+              ? prop.replace("_p_", "")
+              : prop;
+
+            // Get each value from props and assign it to the cleaned property
+            thing[cleanProp] = element.get(prop);
+          }
+
+          result[i] = thing;
+        }
+
+        return result;
+      },
     };
   }
 })();
